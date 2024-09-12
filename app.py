ea8803da4f90750a2eafa3ad62a3e3755c1b9420
Atiq Mansoori <er_asm@outlook.com>
2024-09-12 09:47:55 +0530

    Updated Requirements

---
+++ app.py
@@ -0,0 +1,9 @@
+
+
+# Usage
+input_text_file = 'quote.txt'  # Path to the text file containing the quote
+output_image_file = 'quote_image.png'  # Path to save the generated image
+
+create_image_with_quote(input_text_file, output_image_file)
+
+print(f"Image created and saved as {output_image_file}")
--- utils.py
+++ utils.py
@@ -35,10 +35,3 @@ def create_image_with_quote(input_text_file, output_image_file, image_size=(1080
     # Save the image to a file
     image.save(output_image_file)
 
-# Usage
-input_text_file = 'quote.txt'  # Path to the text file containing the quote
-output_image_file = 'quote_image.png'  # Path to save the generated image
-
-create_image_with_quote(input_text_file, output_image_file)
-
-print(f"Image created and saved as {output_image_file}")
